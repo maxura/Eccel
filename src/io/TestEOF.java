e4329553f5d9f1ffb495eb68dcdd5d43f8e34e22
maxura <maxura@ukr.net>
2013-11-01 00:51:05 +0200

    try add files

--- src/io/TestEOF.java
+++ src/io/TestEOF.java
@@ -1,13 +1,17 @@
-//: io/TestEOF.java
+package io;//: io/TestEOF.java
 // Testing for end of file while reading a byte at a time.
+
+import java.io.IOException;
+
+
 import java.io.*;
 
 public class TestEOF {
   public static void main(String[] args)
   throws IOException {
     DataInputStream in = new DataInputStream(
-      new BufferedInputStream(
-        new FileInputStream("TestEOF.java")));
+
+        new FileInputStream("TestEOF.java"));
     while(in.available() != 0)
       System.out.print((char)in.readByte());
   }
