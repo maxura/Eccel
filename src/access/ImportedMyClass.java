91bf28a998f465fba0dbc17b3f4e463a89a0a72f
Maxim Musienko <mmusienko@codenvy.com>
2014-07-10 15:33:57 +0300

    fit ti maven standarts

--- src/access/ImportedMyClass.java
+++
@@ -1,9 +0,0 @@
-package access;//: access/ImportedMyClass.java
-
-import access.mypackage.MyClass;
-
-public class ImportedMyClass {
-    public static void main(String[] args) {
-        MyClass m = new MyClass();
-    }
-} ///:~
--- src/swt/util/SWTApplication.java
+++
@@ -1,9 +0,0 @@
-//: swt/util/SWTApplication.java
-package swt.util;
-//import org.eclipse.swt.widgets.*;
-
-import java.awt.*;
-
-public interface SWTApplication {
-  void createContents(Composite parent);
-} ///:~
