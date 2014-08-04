ea9e57779c3211c3d82e86d785def1f419123ddf
Maxim Musienko <mmusienko@codenvy.com>
2014-08-04 17:15:38 +0300

    fit ti maven standarts

--- src/main/java/ua/max/pochercushki/SomeClass4.java
+++ src/main/java/ua/max/pochercushki/SomeClass4.java
@@ -4,12 +4,16 @@ package ua.max.pochercushki;
  * @author <a href="mailto:mmusienko@codenvy.com">Musienko Maxim</a>
  * @version $Id: 2013 8/22/13 10:17 AM
  */
-public class SomeClass4 {
+public class SomeClass4  extends SomeClass{
     static         String gitUrl      = "https://github.com/idexmai/mavenProjectIDEX.git";
     private static String nameProject = "CustomizeProjectName";
     private static String factoryData =
             "{\"v\":\"1.2\",\"vcs\":\"git\",\"vcsurl\":\"" + gitUrl + "\"," + "\"projectattributes\": {\"pname\":" + "\"" + nameProject +
             "\"" + "}}";
+
+    public SomeClass4(Integer ar, Float b) {
+        super(ar, b);
+    }
 
 
     private static String convertLoginToPass(String login) {
