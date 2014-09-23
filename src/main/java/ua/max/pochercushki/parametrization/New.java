573e8820f9ff73de88902f864354d4345de015d9
Maxim Musienko <mmusienko@codenvy.com>
2014-09-23 18:07:14 +0300

    fit ti maven standarts

---
+++ src/main/java/ua/max/pochercushki/parametrization/New.java
@@ -0,0 +1,37 @@
+package ua.max.pochercushki.parametrization;
+
+import java.util.*;
+
+/**
+ * Created by mmusienko on 9/23/14.
+ */
+public class New {
+    public static <K, V> Map<K, V> map() {
+        return new HashMap<K, V>();
+    }
+
+    public static <T> List<T> list() {
+        return new ArrayList<T>();
+    }
+
+
+    public static <T> LinkedList<T> llist() {
+        return new LinkedList<T>();
+    }
+
+    public static <T> Set<T> set() {
+        return new HashSet<T>();
+    }
+
+    public static <T> Queue<T> queue() {
+        return new LinkedList<T>();
+    }
+
+    public static void main(String[] args) {
+        Map<String, List<String>> sls = New.map();
+        List<String> ls = New.list();
+        LinkedList<String> lls = New.llist();
+        Set<String> ss = New.set();
+        Queue<String> qs = New.queue();
+    }
+}
