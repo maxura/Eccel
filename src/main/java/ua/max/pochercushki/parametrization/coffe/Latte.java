7056db5a5b6941b974f8175f7e3681876eded180
Maxim Musienko <mmusienko@codenvy.com>
2014-09-23 09:51:10 +0300

    fit ti maven standarts

---
+++ src/main/java/ua/max/pochercushki/parametrization/coffe/Latte.java
@@ -0,0 +1,8 @@
+package ua.max.pochercushki.parametrization.coffe;
+
+/**
+ * Created by mmusienko on 9/22/14.
+ */
+public class Latte extends Coffee {
+
+}
---
+++ src/main/java/ua/max/pochercushki/parametrization/util/Generator.java
@@ -0,0 +1,8 @@
+package ua.max.pochercushki.parametrization.util;
+
+/**
+ * Created by mmusienko on 9/22/14.
+ */
+public interface Generator<T> {
+    T next();
+}
