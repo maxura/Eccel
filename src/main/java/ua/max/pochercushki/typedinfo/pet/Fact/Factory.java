21f61dea6fafa655bfc23f56655e23bde021e8d7
maxura <maxura@ukr.net>
2014-10-07 00:16:01 +0300

    fit ti maven standarts

---
+++ src/main/java/ua/max/pochercushki/typedinfo/pet/Fact/Factory.java
@@ -0,0 +1,12 @@
+package ua.max.pochercushki.typedinfo.pet.Fact;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nata
+ * Date: 06.10.14
+ * Time: 23:57
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Factory<T> {
+    T create();
+}
