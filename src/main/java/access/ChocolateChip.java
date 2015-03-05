2c1308097e7e4633f0baa732be63272464125cba
Anonymous <hbrbvcgs@sharklasers.com>
2015-03-05 17:28:19 +0200

    dwa

--- src/main/java/access/ChocolateChip.java
+++ src/main/java/access/ChocolateChip.java
@@ -1,6 +1,6 @@
 package access;//: access/ChocolateChip.java
 // Can't use package-access member from another package.
-
+//
 import access.dessert.Cookie;
 
 public class ChocolateChip extends Cookie {
